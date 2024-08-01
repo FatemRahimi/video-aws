30a06586a00a0f88af2b5b97a2b08952f1029751
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-08-01 22:52:15 +0100

    Update s3.tf

--- terraform/s3/s3.tf
+++ terraform/s3/s3.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "video_bucket" {
 resource "aws_s3_bucket_public_access_block" "video_bucket" {
   bucket = aws_s3_bucket.video_bucket.id
 
-  block_public_acls       = false
+  block_public_acls       = true
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
